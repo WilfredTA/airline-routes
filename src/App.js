3a647b704b12231df1d4680497ea970310d35bba
Tannr Allard <tallard@localhost.localdomain>
2017-12-15 16:33:15 -0500

    Adding pagination

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import './App.css';
 import DATA from './data.js';
 
 class App extends Component {
+
+
+  formatValue(property, value){
+    if (property==='airline'){
+      return DATA.getAirlineById(value).name;
+    } else {
+      return DATA.getAirportByCode(value).name;
+    }
+  }
+
+
+
+
   render() {
-    const columns = ['Airline', 'Source Airport', 'Destination Airport'];
+    const perPage = 25;
+    const columns = [
+      {name: "Airline", property: 'airline'},
+      {name: "Source Airport", property: 'src'},
+      {name: "Destination Airport", property: 'dest'}
+    ];
     const rows = DATA.routes;
     return (
       <div className="app">
@@ -13,8 +31,11 @@ class App extends Component {
         </header>
         <section>
           <RoutesTable
+            perPage={perPage}
+            className="routes-table"
             columns={columns}
             rows={rows}
+            format={this.formatValue}
             />
         </section>
       </div>
@@ -24,34 +45,70 @@ class App extends Component {
 
 class RoutesTable extends Component {
 
+  constructor(props) {
+    super(props)
+    this.state = {
+      page: 0
+    }
+  };
+
+  nextPage = (event) => {
+      event.preventDefault();
+      console.log('next clicked')
+      this.setState(function(prevState){
+        return {page: prevState.page + 1};
+      });
+    }
+
+    prevPage = (event) => {
+      event.preventDefault();
+      this.setState(function(prevState){
+        return {page: prevState.page - 1};
+      });
+  }
+
+
   render(){
+    const start = this.state.page * this.props.perPage;
     const columns = this.props.columns.map((col) => {
       return (
         <th>
-          {col}
+          {col.name}
         </th>
       )
     })
-    const rows = this.props.rows.map((row) => {
-      return (<tr>
-        <td>{row.airline}</td>
-        <td>{row.src}</td>
-        <td>{row.dest}</td>
+    const rows = this.props.rows.slice(start, start + this.props.perPage).map((row) => {
+      return (
+        <tr>
+          <td>{this.props.format('airline', row.airline)}</td>
+          <td>{this.props.format('airport', row.src)}</td>
+          <td>{this.props.format('airport', row.dest)}</td>
         </tr>
       );
     });
 
     return (
-      <table>
-        <thead>
-          <tr>
-            {columns}
-          </tr>
-        </thead>
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
+      <div>
+        <table className={this.props.className}>
+          <thead>
+            <tr>
+              {columns}
+            </tr>
+          </thead>
+          <tbody>
+            {rows}
+          </tbody>
+        </table>
+      <div>  Showing {start + 1} - {start+25} of {this.props.rows.length} routes</div>
+      <div className="pagination">
+        <button disabled={this.state.page === 0} onClick={this.prevPage}>
+          Previous
+        </button>
+        <button disabled={start + this.props.perPage >= this.props.rows.length} onClick={this.nextPage}>
+          Next
+        </button>
+      </div>
+    </div>
     )
   }
 }
